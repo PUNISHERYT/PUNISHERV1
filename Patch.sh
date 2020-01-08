01bf54d69bd4be2d0036fb3c2364fa61773a0bda
PUNISHERYT <59607476+PUNISHERYT@users.noreply.github.com>
2020-01-08 17:32:40 +0530

    Update Patch.sh

--- Patch.sh
+++ Patch.sh
@@ -1,1 +1,107 @@
-apt update && apt upgradeapt install figlet -yapt install neofetch -yapt install toilet -yapt install wget -yapt install ruby -yapt install tsu -yapt install unzip -ywget https://github.com/busyloop/lolcat/archive/master.zipunzip master.zipcd lolcat-master/bin/gem install lolcatcleartoilet -f mono12 -F gay -t 'PUNISHER' | lolcat -a -d 1toilet -f wideterm -F border:gay -t '    Creater @Official_ShouT    ' | lolcat -a -d 1echo ""toilet -f mini -F border:gay -t 'PUBG GL PATCHER' | lolcat -a -d 3echo ""echo ""toilet -f term -F gay '  Gathering Device Info.... Please wait,'echo ""sleep 3neofetch | lolcat -a -d 3echo""toilet -f term -F metal " PATCHING IN PROGRESS WAIT :-) "tsudo cp /data/data/com.termux/files/home/TeamRoarHacks/res/lib/libabase.so /data/data/com.tencent.ig/lib/tsudo cp /data/data/com.termux/files/home/TeamRoarHacks/res/lib/libBugly.so /data/data/com.tencent.ig/lib/tsudo cp /data/data/com.termux/files/home/TeamRoarHacks/res/lib/libcubehawk.so /data/data/com.tencent.ig/lib/tsudo cp /data/data/com.termux/files/home/TeamRoarHacks/res/lib/libgcloud.so /data/data/com.tencent.ig/lib/tsudo cp /data/data/com.termux/files/home/TeamRoarHacks/res/lib/libGCloudVoice.so /data/data/com.tencent.ig/lib/tsudo cp /data/data/com.termux/files/home/TeamRoarHacks/res/lib/libgnustl_shared.so /data/data/com.tencent.ig/lib/tsudo cp /data/data/com.termux/files/home/TeamRoarHacks/res/lib/libhelpshiftlistener.so /data/data/com.tencent.ig/lib/tsudo cp /data/data/com.termux/files/home/TeamRoarHacks/res/lib/libigshare.so /data/data/com.tencent.ig/lib/tsudo cp /data/data/com.termux/files/home/TeamRoarHacks/res/lib/libIMSDK.so /data/data/com.tencent.ig/lib/tsudo cp /data/data/com.termux/files/home/TeamRoarHacks/res/lib/liblbs.so /data/data/com.tencent.ig/lib/tsudo cp /data/data/com.termux/files/home/TeamRoarHacks/res/lib/libTDataMaster.so /data/data/com.tencent.ig/lib/tsudo cp /data/data/com.termux/files/home/TeamRoarHacks/res/lib/libtersafe.so /data/data/com.tencent.ig/lib/tsudo cp /data/data/com.termux/files/home/TeamRoarHacks/res/lib/libtprt.so /data/data/com.tencent.ig/lib/tsudo cp /data/data/com.termux/files/home/TeamRoarHacks/res/lib/libUE4.so /data/data/com.tencent.ig/lib/tsudo cp /data/data/com.termux/files/home/TeamRoarHacks/res/lib/libzip.so /data/data/com.tencent.ig/lib/tsudo cp /data/data/com.termux/files/home/TeamRoarHacks/res/lib/libzlib.so /data/data/com.tencent.ig/lib/chmod -R 755 /data/data/com.tencent.ig/lib/libabase.sochmod -R 755 /data/data/com.tencent.ig/lib/libBugly.sochmod -R 755 /data/data/com.tencent.ig/lib/libcubehawk.sochmod -R 755 /data/data/com.tencent.ig/lib/libgcloud.sochmod -R 755 /data/data/com.tencent.ig/lib/libGCloudVoice.sochmod -R 755 /data/data/com.tencent.ig/lib/libgnustl_shared.sochmod -R 755 /data/data/com.tencent.ig/lib/libhelpshiftlistener.sochmod -R 755 /data/data/com.tencent.ig/lib/libigshare.sochmod -R 755 /data/data/com.tencent.ig/lib/libIMSDK.sochmod -R 755 /data/data/com.tencent.ig/lib/liblbs.sochmod -R 755 /data/data/com.tencent.ig/lib/libTDataMaster.sochmod -R 755 /data/data/com.tencent.ig/lib/libtersafe.sochmod -R 755 /data/data/com.tencent.ig/lib/libtprt.sochmod -R 755 /data/data/com.tencent.ig/lib/libUE4.sochmod -R 755 /data/data/com.tencent.ig/lib/libzip.sochmod -R 755 /data/data/com.tencent.ig/lib/libzlib.sosleep 5toilet -f mono12 'DONE' | lolcat -a -d 10toilet -f mini -F gay 'READ CAREFULLY:'toilet -f term -F metal 'All Patching Process is completed'toilet -f term -F gay '1-> Have Patient  'toilet -f term -F gay '2-> FIRSTLY, Try on Guest, Main acc. never suggested to try new Hacks'toilet -f term -F gay '3-> JOIN @PUNISHER_YT on Telegram'toilet -f term -F gay '4-> Only GG + Game'toilet -f term -F metal:border 'If you see any error during execution, Contact ADMIN ASAP!'echo ""echo ""toilet -f term -F border:gay -t 'Broght to you by : Telegram : t.me/PUNISHER_YT'toilet -f wideterm -F border -t ' Creator : @Official_ShouT ' | lolcat -a -d 5toilet -f term -F border:gay -t 'Also support : @PUNISHER_YT' | lolcat -a -d 1toilet -f term -F gay '#Launching PUBGM'sleep 10am start -n com.tencent.ig/com.epicgames.ue4.SplashActivityexit
+
+toilet -f mono12 -F gay -t 'PUNISHER' | lolcat -a -d 1
+
+toilet -f wideterm -F border:gay -t '    Creater @Official_ShouT    ' | lolcat -a -d 1
+
+echo ""
+
+toilet -f mini -F border:gay -t 'PUBG GL PATCHER' | lolcat -a -d 3
+
+echo ""
+
+echo ""
+
+toilet -f term -F gay '  Gathering Device Info.... Please wait,'
+
+echo ""
+
+sleep 3
+
+neofetch | lolcat -a -d 3
+
+echo""
+
+toilet -f term -F metal " PATCHING IN PROGRESS WAIT :-) "
+
+tsudo cp /data/data/com.termux/files/home/TeamRoarHacks/res/lib/libabase.so /data/data/com.tencent.ig/lib/
+
+tsudo cp /data/data/com.termux/files/home/TeamRoarHacks/res/lib/libBugly.so /data/data/com.tencent.ig/lib/
+
+tsudo cp /data/data/com.termux/files/home/TeamRoarHacks/res/lib/libcubehawk.so /data/data/com.tencent.ig/lib/
+
+tsudo cp /data/data/com.termux/files/home/TeamRoarHacks/res/lib/libgcloud.so /data/data/com.tencent.ig/lib/
+
+tsudo cp /data/data/com.termux/files/home/TeamRoarHacks/res/lib/libGCloudVoice.so /data/data/com.tencent.ig/lib/
+
+tsudo cp /data/data/com.termux/files/home/TeamRoarHacks/res/lib/libgnustl_shared.so /data/data/com.tencent.ig/lib/
+
+tsudo cp /data/data/com.termux/files/home/TeamRoarHacks/res/lib/libhelpshiftlistener.so /data/data/com.tencent.ig/lib/
+
+tsudo cp /data/data/com.termux/files/home/TeamRoarHacks/res/lib/libigshare.so /data/data/com.tencent.ig/lib/
+
+tsudo cp /data/data/com.termux/files/home/TeamRoarHacks/res/lib/libIMSDK.so /data/data/com.tencent.ig/lib/
+
+tsudo cp /data/data/com.termux/files/home/TeamRoarHacks/res/lib/liblbs.so /data/data/com.tencent.ig/lib/
+
+tsudo cp /data/data/com.termux/files/home/TeamRoarHacks/res/lib/libTDataMaster.so /data/data/com.tencent.ig/lib/
+
+tsudo cp /data/data/com.termux/files/home/TeamRoarHacks/res/lib/libtersafe.so /data/data/com.tencent.ig/lib/
+
+tsudo cp /data/data/com.termux/files/home/TeamRoarHacks/res/lib/libtprt.so /data/data/com.tencent.ig/lib/
+
+tsudo cp /data/data/com.termux/files/home/TeamRoarHacks/res/lib/libUE4.so /data/data/com.tencent.ig/lib/
+
+tsudo cp /data/data/com.termux/files/home/TeamRoarHacks/res/lib/libzip.so /data/data/com.tencent.ig/lib/
+
+tsudo cp /data/data/com.termux/files/home/TeamRoarHacks/res/lib/libzlib.so /data/data/com.tencent.ig/lib/
+
+chmod -R 755 /data/data/com.tencent.ig/lib/libabase.so
+chmod -R 755 /data/data/com.tencent.ig/lib/libBugly.so
+chmod -R 755 /data/data/com.tencent.ig/lib/libcubehawk.so
+chmod -R 755 /data/data/com.tencent.ig/lib/libgcloud.so
+chmod -R 755 /data/data/com.tencent.ig/lib/libGCloudVoice.so
+chmod -R 755 /data/data/com.tencent.ig/lib/libgnustl_shared.so
+chmod -R 755 /data/data/com.tencent.ig/lib/libhelpshiftlistener.so
+chmod -R 755 /data/data/com.tencent.ig/lib/libigshare.so
+chmod -R 755 /data/data/com.tencent.ig/lib/libIMSDK.so
+chmod -R 755 /data/data/com.tencent.ig/lib/liblbs.so
+chmod -R 755 /data/data/com.tencent.ig/lib/libTDataMaster.so
+chmod -R 755 /data/data/com.tencent.ig/lib/libtersafe.so
+chmod -R 755 /data/data/com.tencent.ig/lib/libtprt.so
+chmod -R 755 /data/data/com.tencent.ig/lib/libUE4.so
+chmod -R 755 /data/data/com.tencent.ig/lib/libzip.so
+chmod -R 755 /data/data/com.tencent.ig/lib/libzlib.so
+sleep 5
+
+toilet -f mono12 'DONE' | lolcat -a -d 10
+
+toilet -f mini -F gay 'READ CAREFULLY:'
+
+toilet -f term -F metal 'All Patching Process is completed'
+
+toilet -f term -F gay '1-> Have Patient  '
+
+toilet -f term -F gay '2-> FIRSTLY, Try on Guest, Main acc. never suggested to try new Hacks'
+
+toilet -f term -F gay '3-> JOIN @PUNISHER_YT on Telegram'
+
+toilet -f term -F gay '4-> Only GG + Game'
+
+toilet -f term -F metal:border 'If you see any error during execution, Contact ADMIN ASAP!'
+
+echo ""
+
+echo ""
+
+toilet -f term -F border:gay -t 'Broght to you by : Telegram : t.me/PUNISHER_YT'
+
+toilet -f wideterm -F border -t ' Creator : @Official_ShouT ' | lolcat -a -d 5
+
+toilet -f term -F border:gay -t 'Also support : @PUNISHER_YT' | lolcat -a -d 1
+
+toilet -f term -F gay '#Launching PUBGM'
+
+sleep 10
+am start -n com.tencent.ig/com.epicgames.ue4.SplashActivity
+
+exit
